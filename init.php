ff65393a3d5b65344b09f2dd4f58fd347cf6e7bb
Berdnikov Alexey <aberdnikov@gmail.com>
2016-06-29 12:13:33 +0500

    Create init.php

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-angularjs-toaster')
+    ->usePackage('larakit/sf-angular')
+    ->js('//cdnjs.cloudflare.com/ajax/libs/angularjs-toaster/2.0.0/toaster.min.js')
+    ->css('//cdnjs.cloudflare.com/ajax/libs/angularjs-toaster/2.0.0/toaster.min.css');
