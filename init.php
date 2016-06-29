7147d936cd9d0276c2d3267d24064ba39173d88e
Alexey Berdnikov <aberdnikov@gmail.com>
2016-06-29 21:12:19 +0500

    fixed

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angularjs-toaster')
     ->usePackage('larakit/sf-angular')
+    ->ngModule('toaster')
     ->js('//cdnjs.cloudflare.com/ajax/libs/angularjs-toaster/2.0.0/toaster.min.js')
     ->css('//cdnjs.cloudflare.com/ajax/libs/angularjs-toaster/2.0.0/toaster.min.css');
 
