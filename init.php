a5cac664b9feefcd1aca4b3019d6bbf8cd60323f
Alexey Berdnikov <aberdnikov@gmail.com>
2016-06-29 12:29:15 +0500

    fixed

--- init.php
+++ init.php
@@ -3,3 +3,5 @@
     ->usePackage('larakit/sf-angular')
     ->js('//cdnjs.cloudflare.com/ajax/libs/angularjs-toaster/2.0.0/toaster.min.js')
     ->css('//cdnjs.cloudflare.com/ajax/libs/angularjs-toaster/2.0.0/toaster.min.css');
+
+\Larakit\Page\Page::bodyAppend('<toaster-container></toaster-container>');
